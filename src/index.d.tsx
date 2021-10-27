6c2343e4e60bc3b792c79848bbc04c57ef42f38a
fbogatko <f.bogatko@clearcode.cc>
2021-10-27 13:29:23 +0200

    PRNS-6 Custom events documentation and unit tests

--- src/index.d.tsx
+++ src/index.d.tsx
@@ -10,14 +10,15 @@ type PiwikProSdkType = {
   /**
    * Sends tracking screen view event.
    * @path screen path
-   * @options tracking screen view options (title, customDimensions, screenCustomVariables, visitCustomVariables)
+   * @options tracking screen options (title, customDimensions, screenCustomVariables, visitCustomVariables)
    */
   trackScreen(path: string, options?: TrackScreenOptions): Promise<void>;
 
   /**
    * Tracks custom event.
-   * @path screen path
-   * @options tracking screen view options (title, customDimensions, screenCustomVariables, visitCustomVariables)
+   * @category event category
+   * @action specific event action within the category specified
+   * @options custom event options (name, value, path, customDimensions, visitCustomVariables)
    */
   trackCustomEvent(
     category: string,
