1f34bf8ceaab737e048fd9303f105bfd5ea54559
fbogatko <f.bogatko@clearcode.cc>
2022-02-07 12:35:05 +0100

    PRNS-19 Visitor ID getter on iOS

--- ios/PiwikProSdk.m
+++ ios/PiwikProSdk.m
@@ -426,6 +426,23 @@ RCT_REMAP_METHOD(setVisitorId,
     }
 }
 
+RCT_REMAP_METHOD(getVisitorId,
+                 getVisitorIdWithResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+{
+    if ([PiwikTracker sharedInstance] == nil) {
+        reject(@"not_initialized", @"Piwik Pro SDK has not been initialized", nil);
+        return;
+    }
+    
+    @try {
+        NSString* visitorId = [[PiwikTracker sharedInstance] visitorID];
+        resolve(visitorId);
+    } @catch (NSException *exception) {
+        reject(exception.name, exception.reason, nil);
+    }
+}
+
 RCT_REMAP_METHOD(setSessionTimeout,
                  setSessionTimeoutWithSessionTimeout:(double)sessionTimeout
                  withResolver:(RCTPromiseResolveBlock)resolve
