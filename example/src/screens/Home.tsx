1c2a2b56b243ec2e7ea6e95fbff3eb84c1287891
fbogatko <f.bogatko@clearcode.cc>
2022-03-03 12:05:13 +0100

    PRNS-36 Package name changed

--- example/src/screens/Home.tsx
+++ example/src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import PiwikProSdk from 'react-native-piwik-pro-sdk';
+import PiwikProSdk from '@piwikpro/react-native-piwik-pro-sdk';
 import { Button, ScrollViewContainer } from '../components';
 import {
   setDispatchInterval,
@@ -19,8 +19,8 @@ export default function Home({ navigation }: Props) {
 
   const initializePiwikProSdk = async () => {
     await PiwikProSdk.init(
-      'https://your.piwik.pro.server.com',
-      '01234567-89ab-cdef-0123-456789abcdef'
+      'https://astralprojection.promilci.com',
+      '0bcdeb8b-8055-49ec-8425-b45ce73c46a6'
     )
       .then(() => dispatch(setMessage('Success')))
       .catch((error) => dispatch(setError(error.message)));
