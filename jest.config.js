dc6c7564b74eefe8cafe4ad8f3adf55e04cfd64b
Andrzej Mironowicz <a.mironowicz@piwik.pro>
2026-03-26 05:51:46 +0100

    fix: simplify jest config to fix CI transform resolution

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  transform: {
-    '^.+\\.(js|ts|tsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)',
-  ],
   modulePathIgnorePatterns: [
     '<rootDir>/example/node_modules',
     '<rootDir>/lib/',
