b353c6eadc27f6460a5e943175aa89e3802f5aa3
Andrzej Mironowicz <a.mironowicz@piwik.pro>
2026-09-09 17:15:00 +0200

    chore: update dependencies and tooling

--- example/android/app/src/main/java/com/example/reactnativepiwikprosdk/MainApplication.kt
+++ example/android/app/src/main/java/com/example/reactnativepiwikprosdk/MainApplication.kt
@@ -5,25 +5,17 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
 import com.reactnativepiwikprosdk.PiwikProSdkPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages() =
-            PackageList(this).packages.apply { add(PiwikProSdkPackage()) }
-
-        override fun getJSMainModuleName(): String = "index"
-
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+  override val reactHost: ReactHost by lazy {
+    getDefaultReactHost(
+      context = applicationContext,
+      packageList = PackageList(this).packages.apply { add(PiwikProSdkPackage()) },
+    )
+  }
 
   override fun onCreate() {
     super.onCreate()
